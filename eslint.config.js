2aa62ae0cb5d4d0eac925911462b906cff92c896
jamesd810 <jdobson810@gmail.com>
2025-04-10 21:05:03 -0500

    Added eslint config

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+// import { defineConfig } from "eslint/config";
+
+// export default defineConfig([
+//   {
+//     rules: {
+//       eqeqeq: "off",
+//       "no-unused-vars": "error",
+//       "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+//     },
+//   },
+// ]);
