445a4377122d92f01103334323bb89c832425241
jamesd810 <jdobson810@gmail.com>
2025-07-02 21:31:47 -0500

    Added styling to Services page

--- src/components/cleaningServices/StandardCleaning.tsx
+++ src/components/cleaningServices/StandardCleaning.tsx
@@ -3,52 +3,56 @@ import "../../pages/Services/servicesPage.scss";
 const StandardCleaning = (): React.JSX.Element => {
   return (
     <div className="services-page">
-      <section className="service-category">
-        <h3>Kitchen</h3>
-        <ul>
-          <li>Wipe Fridge</li>
-          <li>Clean Countertops</li>
-          <li>Wipe Appliances</li>
-          <li>Clean Sink</li>
-          <li>Clean Stovetop</li>
-          <li>Microwave (inside)</li>
-          <li>Cabinets (inside)</li>
-          <li>Mop floor</li>
-        </ul>
-      </section>
+      <div className="sectionContainer">
+        <section className="service-category">
+          <h3>Kitchen</h3>
+          <ul>
+            <li>Wipe Fridge</li>
+            <li>Clean Countertops</li>
+            <li>Wipe Appliances</li>
+            <li>Clean Sink</li>
+            <li>Clean Stovetop</li>
+            <li>Microwave (inside)</li>
+            <li>Cabinets (inside)</li>
+            <li>Mop floor</li>
+          </ul>
+        </section>
 
-      <section className="service-category">
-        <h3>Bathrooms</h3>
-        <ul>
-          <li>Clean Countertops</li>
-          <li>Scrub Toilets</li>
-          <li>Clean Sink</li>
-          <li>Rinse & Wipe Tub</li>
-          <li>Rinse & Wipe Shower</li>
-        </ul>
-      </section>
+        <section className="service-category">
+          <h3>Bathrooms</h3>
+          <ul>
+            <li>Clean Countertops</li>
+            <li>Scrub Toilets</li>
+            <li>Clean Sink</li>
+            <li>Rinse & Wipe Tub</li>
+            <li>Rinse & Wipe Shower</li>
+          </ul>
+        </section>
+      </div>
 
-      <section className="service-category">
-        <h3>All Areas</h3>
-        <ul>
-          <li>Empty Bins</li>
-          <li>Vacuum & Mop</li>
-          <li>Dust All Surfaces</li>
-          <li>Clean Baseboards</li>
-          <li>Clean Window Ledges</li>
-        </ul>
-      </section>
+      <div className="sectionContainer">
+        <section className="service-category">
+          <h3>All Areas</h3>
+          <ul>
+            <li>Empty Bins</li>
+            <li>Vacuum & Mop</li>
+            <li>Dust All Surfaces</li>
+            <li>Clean Baseboards</li>
+            <li>Clean Window Ledges</li>
+          </ul>
+        </section>
 
-      <section className="service-category">
-        <h3>Bedrooms</h3>
-        <ul>
-          <li>Quick De-clutter</li>
-          <li>Make Beds</li>
-          <li>Dust All Surfaces</li>
-          <li>Wipe Tables</li>
-          <li>Change Sheets (request)</li>
-        </ul>
-      </section>
+        <section className="service-category">
+          <h3>Bedrooms</h3>
+          <ul>
+            <li>Quick De-clutter</li>
+            <li>Make Beds</li>
+            <li>Dust All Surfaces</li>
+            <li>Wipe Tables</li>
+            <li>Change Sheets (request)</li>
+          </ul>
+        </section>
+      </div>
     </div>
   );
 };
