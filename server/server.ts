17ab03c02a5e3f6a07986a9391f14efcf6cd9794
jamesd810 <jdobson810@gmail.com>
2025-07-29 21:48:34 -0500

    uncomment coded in server.ts

--- server/server.ts
+++ server/server.ts
@@ -3,8 +3,8 @@ import applyRoute from "./middleware/sendApplication.js";
 
 const app = express();
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/api", applyRoute);
 
 const PORT = process.env.PORT || 3000;
