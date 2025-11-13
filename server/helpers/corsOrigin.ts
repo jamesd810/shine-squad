cd665bba2b5ccd41b43b194f610069bb01519171
jamesd810 <jdobson810@gmail.com>
2025-11-12 23:07:51 -0600

    fix: Added url to allowedOrigins array

--- server/helpers/corsOrigin.ts
+++ server/helpers/corsOrigin.ts
@@ -1,11 +1,10 @@
 import cors from "cors";
 
 const allowedOrigins =
-  process.env.NODE_ENV === "production" ? ["https://www.shinesquadchicago.com"] : ["http://localhost:5000"];
+  process.env.NODE_ENV === "production" ? ["https://www.shinesquadchicago.com", "https://shinesquadchicago.com"] : ["http://localhost:5000"];
 
 const corsOrigin = () =>
-  cors({
-    origin: (origin, callback) => {
+  cors({origin: (origin, callback) => {
       if (!origin) return callback(null, true);
       if (allowedOrigins.includes(origin)) {
         return callback(null, true);
