d48e075f4cb26e86b16b6108fd791b3bada1d261
jamesd810 <jdobson810@gmail.com>
2026-02-03 22:43:46 -0600

    Fix: Added constants for form variables

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const nameConstant = "Name is required";
+export const emailConstant = "Email is required";
+export const phoneConstant = "Phone number is required";
+export const resumeConstant = "Resume is required";
+
+export const minNameLengthMessage = "Name must be at least 2 characters";
